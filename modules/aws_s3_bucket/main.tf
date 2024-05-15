0d42e12741121a1e6cdcaa08bbb4be76c5ed3ee1
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-15 19:13:14 +0200

    moved to s3_lambda_listener module

--- modules/aws_s3_bucket/main.tf
+++ modules/aws_s3_bucket/main.tf
@@ -9,5 +9,6 @@ resource "aws_s3_bucket" "mailing_list_bucket" {
 
 resource "aws_s3_object" "mailing_lists" {
   bucket = aws_s3_bucket.mailing_list_bucket.id
-  key    = "mailing_lists"
+  key    = "mailing_lists/"
 }
+
